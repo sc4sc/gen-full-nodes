56da2a0e6d80b17f62181b1d2ac6b70b784a6f77
Yejun Kim <no1expman@gmail.com>
2019-07-08 23:42:24 +0900

    Update environments

--- _run.sh
+++ _run.sh
@@ -1,4 +1,4 @@
-REGISTRY=lambda2.kaist.ac.kr:5000
+REGISTRY=asia.gcr.io/deep-rainfall-236010
 KLAYTN="docker run --rm -v $(pwd)/{data_dir}:/data/{data_dir} -p {rpc_port}:{rpc_port} -p {port}:{port} -p {ws_port}:{ws_port} $REGISTRY/klaytn /klay"
 # KLAYTN=./klay
 $KLAYTN --nodetype {node_type} --mine --srvtype http --dbtype leveldb --verbosity 3 --txpool.accountslots 20000 --txpool.accountqueue 20000 --txpool.globalslots 40960 --txpool.globalqueue 40960 --networkid 2018 --nodiscover --wsorigins "*" --datadir /data/{data_dir} --syncmode full --rpc --rpcapi admin,txpool,personal,klay,debug,net,web3 --ws --wsaddr 0.0.0.0 --wsport {ws_port} --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcvhosts "*" --maxpeers 5000 --rpcport {rpc_port} --port {port} --gasprice 0
